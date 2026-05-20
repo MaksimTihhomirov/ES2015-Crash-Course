f0ee9bf5ded87195cf21e9ade589ba3b837b8aac
Maksim <maksim.tihhomirov@techno.ee>
2026-05-20 23:46:19 +0300

    Module Bunding With Rollup

---
+++ Module Bundling With Rollup/main.dist.js
@@ -0,0 +1,18 @@
+class TaskCollection{
+
+    constructor(tasks =[]) {
+        this.tasks = tasks;
+    }
+
+    dump() {
+        console.log(this.tasks);
+    }
+
+}
+
+new TaskCollection([
+    'Go to the store',
+    'Finish screencast',
+    'Eat cake'
+
+]).dump();
