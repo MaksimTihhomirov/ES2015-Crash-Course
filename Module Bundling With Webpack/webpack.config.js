ee6180f72fa7c30c273d8a0c2c62ad36cd4cebdd
Maksim <maksim.tihhomirov@techno.ee>
2026-05-21 12:37:21 +0300

    Module Bunding With Webpack

---
+++ Module Bundling With Webpack/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    watch: false,
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
---
+++ Module Bundling With Webpack/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100">
+
+        <style>
+
+            html,body{
+                height: 100px;
+            }
+
+            body {
+                margin: 0;
+                padding:0;
+                width: 100px;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato'
+            }
+
+            container {
+                text-allign: center;
+                display: table-cell;
+                vertical-allign: middle;
+            }
+
+            .content {
+                text-allign: center;
+                display: inline-block;
+
+            }
+
+            .title {
+                font-size: 96px;
+
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="title">Laravel 5</div>
+
+        </div>
+        
+    </body>
+</html>
