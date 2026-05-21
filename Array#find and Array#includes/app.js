fe512097cb28155bd0108c9349031d2b3e6a90aa
Maksim <maksim.tihhomirov@techno.ee>
2026-05-21 12:54:37 +0300

    Array#find and Array#includes

---
+++ Array#find and Array#includes/app.js
@@ -0,0 +1,16 @@
+class User {
+    constructor(name, isAdmin) {
+        this.name = name;
+        this.isAdmin = isAdmin;
+    }
+}
+
+let users = [
+    new User('Jeffrey', false),
+    new User('Jane', true),
+    new User('Jack', false)
+];
+
+console.log(
+    users.find(user => user.isAdmin).name
+);
